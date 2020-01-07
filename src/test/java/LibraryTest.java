babc93541dfa2a7d7c46cc8e1ed1208f26eb713f
naz_liton <nazrulliton@icloud.com>
2020-01-07 19:21:41 +0000

    book count and book add passed

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
     private Library library;
@@ -10,5 +13,11 @@ public class LibraryTest {
         library = new Library();
         book = new Book("Harry P", "JK", "Fantasy");
         book1 = new Book("Jack Ryan", "Lee Child", "Factual");
+    }
+    @Test
+    public void canAddBooks(){
+        library.addBook(book);
+        library.addBook(book1);
+        assertEquals(2, library.bookCount());
     }
 }
