3ace8cb9ac8bbebf01846ab0cb0e72727c227266
naz_liton <nazrulliton@icloud.com>
2020-01-07 18:56:32 +0000

    book class created

---
+++ src/main/java/internalBook.java
@@ -0,0 +1,11 @@
+public class internalBook {
+    private String title;
+    private String author;
+    private String genre;
+
+    public InternalBook (String title, String author, String genre){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+}
